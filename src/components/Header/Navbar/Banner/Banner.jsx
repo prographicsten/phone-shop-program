28e27daa95196874501707167f37c430344cd471
prographicsten <prographicsten@gmail.com>
2023-09-24 11:34:33 +0600

    banner added

---
+++ src/components/Header/Navbar/Banner/Banner.jsx
@@ -0,0 +1,10 @@
+
+const Banner = () => {
+    return (
+        <div className="h-[70vh] bg-green-400 text-center flex items-center justify-center">
+            <h1 className="text-xl">Banner</h1>
+        </div>
+    );
+};
+
+export default Banner;
